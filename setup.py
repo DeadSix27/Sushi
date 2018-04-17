2fd84fd94198460db1f3393bb4df2088f3466125
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-04-17 13:38:43 +0200

    update to python 3
    also replace spaces with tabs

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 import sushi
 
 setup(
-    name='Sushi',
-    description='Automatic subtitle shifter based on audio',
-    version=sushi.VERSION,
-    url='https://github.com/tp7/Sushi',
-    console=['sushi.py'],
-    license='MIT'
+	name='Sushi',
+	description='Automatic subtitle shifter based on audio',
+	version=sushi.VERSION,
+	url='https://github.com/tp7/Sushi',
+	console=['sushi.py'],
+	license='MIT'
 )
 
